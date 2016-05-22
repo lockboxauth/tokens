ecef734782c12c2b2d3ee2d7a6100c48f7219113
Paddy <paddy@impractical.co>
2016-05-21 23:45:52 -0700

    Expose CreatedFrom in apiv1.
    Let apiv1 requests set the CreatedFrom property, and return the
    CreatedFrom property in responses, now that it's a required property for
    requests.

--- apiv1/token.go
+++ apiv1/token.go
@@ -11,13 +11,14 @@ import (
 // RefreshToken is a representation of the tokens.RefreshToken type. It is
 // tooled towards being used in requests and responses for apiv1.
 type RefreshToken struct {
-	ID        string    `json:"id"`
-	CreatedAt time.Time `json:"createdAt"`
-	Scopes    []string  `json:"scopes,omitempty"`
-	ProfileID string    `json:"profileID"`
-	ClientID  string    `json:"clientID"`
-	Revoked   bool      `json:"revoked"`
-	Used      bool      `json:"used"`
+	ID          string    `json:"id"`
+	CreatedAt   time.Time `json:"createdAt"`
+	CreatedFrom string    `json:"createdFrom"`
+	Scopes      []string  `json:"scopes,omitempty"`
+	ProfileID   string    `json:"profileID"`
+	ClientID    string    `json:"clientID"`
+	Revoked     bool      `json:"revoked"`
+	Used        bool      `json:"used"`
 }
 
 // RefreshTokenChange is a representation of the tokens.RefreshTokenChange type.
@@ -33,25 +34,27 @@ type RefreshTokenChange struct {
 
 func coreToken(token RefreshToken) tokens.RefreshToken {
 	return tokens.RefreshToken{
-		ID:        token.ID,
-		CreatedAt: token.CreatedAt,
-		Scopes:    pqarrays.StringArray(token.Scopes),
-		ProfileID: token.ProfileID,
-		ClientID:  token.ClientID,
-		Revoked:   token.Revoked,
-		Used:      token.Used,
+		ID:          token.ID,
+		CreatedAt:   token.CreatedAt,
+		CreatedFrom: token.CreatedFrom,
+		Scopes:      pqarrays.StringArray(token.Scopes),
+		ProfileID:   token.ProfileID,
+		ClientID:    token.ClientID,
+		Revoked:     token.Revoked,
+		Used:        token.Used,
 	}
 }
 
 func apiToken(token tokens.RefreshToken) RefreshToken {
 	return RefreshToken{
-		ID:        token.ID,
-		CreatedAt: token.CreatedAt,
-		Scopes:    []string(token.Scopes),
-		ProfileID: token.ProfileID,
-		ClientID:  token.ClientID,
-		Revoked:   token.Revoked,
-		Used:      token.Used,
+		ID:          token.ID,
+		CreatedAt:   token.CreatedAt,
+		CreatedFrom: token.CreatedFrom,
+		Scopes:      []string(token.Scopes),
+		ProfileID:   token.ProfileID,
+		ClientID:    token.ClientID,
+		Revoked:     token.Revoked,
+		Used:        token.Used,
 	}
 }
 

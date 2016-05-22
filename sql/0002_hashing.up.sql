b8a0af46e0eb8a1c957dac63679b4d326d90b24f
Paddy <paddy@impractical.co>
2016-05-22 01:04:46 -0700

    Set default values when running migrations.
    Our hashing migration left previously-created tokens with a NULL value
    for the hash and salt, because it didn't provide a default, but it also
    forbid NULL values. Now we set them to blank, so the migration won't
    fail due to internal contradictions.

--- sql/0002_hashing.up.sql
+++ sql/0002_hashing.up.sql
@@ -1,4 +1,4 @@
-ALTER TABLE tokens ADD COLUMN hash VARCHAR(64) NOT NULL,
-		   ADD COLUMN hash_salt VARCHAR(64) NOT NULL,
-		   ADD COLUMN hash_iterations INT NOT NULL,
+ALTER TABLE tokens ADD COLUMN hash VARCHAR(64) NOT NULL DEFAULT '',
+		   ADD COLUMN hash_salt VARCHAR(64) NOT NULL DEFAULT '',
+		   ADD COLUMN hash_iterations INT NOT NULL DEFAULT 0,
 		   ADD CONSTRAINT unique_value UNIQUE(hash, hash_salt, hash_iterations);

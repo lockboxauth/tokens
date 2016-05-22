b295fa77bccc41fd31ee5abfbd3d6ae64a6f5f98
Paddy <paddy@impractical.co>
2016-05-21 17:56:04 -0700

    Pass GOOS and GOARCH through when building.
    Update build.sh to pass through the GOOS/GOARCH environment variables
    when the binary is being built, so we can use the script to build for
    OSes and architectures other than the builder's.

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
-
 COMMIT=$(git rev-parse --short HEAD)
 TAG=$(git name-rev --tags --name-only $COMMIT)
 
-go build -o ./tokensd/tokensd -ldflags "-X darlinggo.co/tokens.Version=${TAG} -X darlinggo.co/tokens.Hash=${COMMIT}" ./tokensd
+GOOS=$GOOS GOARCH=$GOARCH go build -o ./tokensd/tokensd -ldflags "-X darlinggo.co/tokens.Version=${TAG} -X darlinggo.co/tokens.Hash=${COMMIT}" ./tokensd

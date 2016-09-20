e765fcb69aecfcf374a9749611c1e9b28f9c53bf
Paddy <paddy@impractical.co>
2016-09-20 01:48:43 -0700

    Use api.NegotiateMiddleware.
    Use the content-type negotiation middleware that darlinggo.co/api
    provides, to let the client know when we can’t respond with the
    content-type they'd like.

--- apiv1/endpoints.go
+++ apiv1/endpoints.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/http"
 
+	"darlinggo.co/api"
 	"darlinggo.co/trout"
 )
 
@@ -12,9 +13,10 @@ import (
 func (a APIv1) Server(ctx context.Context, baseURL string) http.Handler {
 	var router trout.Router
 	router.SetPrefix(baseURL)
-	router.Endpoint("/").Methods("POST").Handler(http.HandlerFunc(a.handleInsertToken))
-	router.Endpoint("/{id}").Methods("GET").Handler(http.HandlerFunc(a.handleGetToken))
-	router.Endpoint("/{id}").Methods("PATCH").Handler(http.HandlerFunc(a.handlePatchToken))
+	router.Endpoint("/").Methods("POST").Handler(api.NegotiateMiddleware(http.HandlerFunc(a.handleInsertToken)))
+	router.Endpoint("/{id}").Methods("GET").Handler(api.NegotiateMiddleware(http.HandlerFunc(a.handleGetToken)))
+	router.Endpoint("/{id}").Methods("PATCH").Handler(api.NegotiateMiddleware(http.HandlerFunc(a.handlePatchToken)))
+	router.Endpoint("/{id}").Methods("POST").Handler(api.NegotiateMiddleware(http.HandlerFunc(a.handlePostToken)))
 
 	return router
 }

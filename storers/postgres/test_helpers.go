106710d1ec252aaedb614b14118d8646117ee69d
Paddy <paddy@impractical.co>
2019-08-13 05:32:04 -0700

    Use TestConnStringEnvVar in postgres test helpers.
    We have a constant defining what environment variable needs to be set to
    the connection string for postgres tests to be run, but we weren't using
    it in the test helpers, we were still hardcoding the value. This just
    substitutes in the proper constant instead of hardcoding the values.

--- storers/postgres/test_helpers.go
+++ storers/postgres/test_helpers.go
@@ -31,13 +31,13 @@ func NewFactory(db *sql.DB) *Factory {
 }
 
 func (f *Factory) NewStorer(ctx context.Context) (tokens.Storer, error) {
-	u, err := url.Parse(os.Getenv("PG_TEST_DB"))
+	u, err := url.Parse(os.Getenv(TestConnStringEnvVar))
 	if err != nil {
-		log.Printf("Error parsing PG_TEST_DB as a URL: %+v\n", err)
+		log.Printf("Error parsing "+TestConnStringEnvVar+" as a URL: %+v\n", err)
 		return nil, err
 	}
 	if u.Scheme != "postgres" {
-		return nil, errors.New("PG_TEST_DB must begin with postgres://")
+		return nil, errors.New(TestConnStringEnvVar + " must begin with postgres://")
 	}
 
 	databaseSuffix, err := uuid.GenerateRandomBytes(6)
